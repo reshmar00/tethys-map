d1e7930833545463785524f771cbd4cc8ab1ede7
reshma raghavan <u1430631@umail.utah.edu>
2025-11-25 19:58:59 -0700

    Replaced the default home page with a Map layout

--- tethysapp-hello_world/tethysapp/hello_world/controllers.py
+++ tethysapp-hello_world/tethysapp/hello_world/controllers.py
@@ -1,75 +1,11 @@
+from tethys_sdk.layouts import MapLayout
 from tethys_sdk.routing import controller
-from tethys_sdk.gizmos import Button
 from .app import App
 
 
-@controller
-def home(request):
-    """
-    Controller for the app home page.
-    """
-    save_button = Button(
-        display_text='',
-        name='save-button',
-        icon='save',
-        style='success',
-        attributes={
-            'data-bs-toggle': 'tooltip',
-            'data-bs-placement': 'top',
-            'title': 'Save'
-        }
-    )
-
-    edit_button = Button(
-        display_text='',
-        name='edit-button',
-        icon='pen',
-        style='warning',
-        attributes={
-            'data-bs-toggle': 'tooltip',
-            'data-bs-placement': 'top',
-            'title': 'Edit'
-        }
-    )
-
-    remove_button = Button(
-        display_text='',
-        name='remove-button',
-        icon='trash',
-        style='danger',
-        attributes={
-            'data-bs-toggle': 'tooltip',
-            'data-bs-placement': 'top',
-            'title': 'Remove'
-        }
-    )
-
-    previous_button = Button(
-        display_text='Previous',
-        name='previous-button',
-        attributes={
-            'data-bs-toggle': 'tooltip',
-            'data-bs-placement': 'top',
-            'title': 'Previous'
-        }
-    )
-
-    next_button = Button(
-        display_text='Next',
-        name='next-button',
-        attributes={
-            'data-bs-toggle': 'tooltip',
-            'data-bs-placement': 'top',
-            'title': 'Next'
-        }
-    )
-
-    context = {
-        'save_button': save_button,
-        'edit_button': edit_button,
-        'remove_button': remove_button,
-        'previous_button': previous_button,
-        'next_button': next_button
-    }
-
-    return App.render(request, 'home.html', context)
+@controller(name="home", app_workspace=True)
+class MapLayoutTutorialMap(MapLayout):
+    app = App
+    base_template = f'{App.package}/base.html'
+    map_title = 'Map Layout Tutorial'
+    map_subtitle = 'NOAA-OWP NextGen Model Outputs'
