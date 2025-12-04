c635b559b7bbdb43953e82174cc457e7fd1f6343
reshma raghavan <u1430631@umail.utah.edu>
2025-12-04 14:01:51 -0700

    Added code for new GeoJSON endpoint in controllers.py

--- tethysapp-hello_world/tethysapp/hello_world/controllers.py
+++ tethysapp-hello_world/tethysapp/hello_world/controllers.py
@@ -1,3 +1,6 @@
+import os
+import json
+from django.http import JsonResponse
 from tethys_sdk.layouts import MapLayout
 from tethys_sdk.routing import controller
 from .app import App
@@ -9,3 +12,19 @@ class MapLayoutTutorialMap(MapLayout):
     base_template = f'{App.package}/base.html'
     map_title = 'Map Layout Tutorial'
     map_subtitle = 'NOAA-OWP NextGen Model Outputs'
+
+
+# ---- New GeoJSON endpoint ----
+@controller(name="geojson")
+def serve_geojson(request):
+    """
+    Serve the GeoJSON for the frontend map.
+    """
+    GEOJSON_PATH = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
+        'resource_data-files', 'GeoJSON', 'counties.geojson'
+    )
+
+    with open(GEOJSON_PATH, 'r') as f:
+        data = json.load(f)
+    return JsonResponse(data)
